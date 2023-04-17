e282f52b1086231228031f5789a3868351314b20
Gary Irick <garyirick@gmail.com>
2023-04-17 01:21:26 -0500

    Cleaned up connection string.

--- src/db/getSqlServer.js
+++ src/db/getSqlServer.js
@@ -11,17 +11,11 @@ module.exports = async () => {
     options: {
       enableArithAbort: true,
       encrypt: true
-    },
-    beforeConnect: (bcConfig) => {
-      // The Driver SQL Server Native Client has been removed from SQL Server 2022.
-      // Source https://learn.microsoft.com/en-us/sql/relational-databases/native-client/applications/installing-sql-server-native-client?view=sql-server-ver16
-      // ODBC Driver 17 for SQL Server is tested working well with SQL Server 2019 & 2022 */
-      bcConfig.conn_str = bcConfig.conn_str.replace('SQL Server Native Client 11.0', 'ODBC Driver 17 for SQL Server')
     }
   }
 
   config.options.trustedConnection = true
-  config.connectionString = 'Driver={SQL Server Native Client 11.0};Server={#{server},#{port}};Database={#{database}};Trusted_Connection={#{trusted}};'
+  config.connectionString = 'Driver={ODBC Driver 17 for SQL Server};Server={#{server},#{port}};Database={#{database}};Trusted_Connection={#{trusted}};'
 
   return await sql.connect(config)
 }
