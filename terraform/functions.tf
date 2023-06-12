d2d08a129063199a26ccae8b5748e12dbe406031
Gary Irick <garyirick@gmail.com>
2023-06-12 15:23:29 -0500

    Switched Azure functions to Node 18.

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -32,7 +32,7 @@ resource "azurerm_windows_function_app" "hots_db_functions" {
     application_insights_key = azurerm_application_insights.functions_logs.instrumentation_key
 
     application_stack {
-      node_version = "~16"
+      node_version = "~18"
     }
 
     cors {
