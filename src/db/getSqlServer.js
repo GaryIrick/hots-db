196237e50bbd9b7522df21b4226ebbd51f626cee
Gary Irick <garyirick@gmail.com>
2025-09-25 16:02:18 -0500

    Upgraded to Node 24.

--- src/db/getSqlServer.js
+++ src/db/getSqlServer.js
@@ -1,4 +1,4 @@
-// We use msnodesqlv8 instead of just mssql because we to use Windows authentication against our local server.
+// We use msnodesqlv8 instead of just mssql because we use Windows authentication against our local server.
 const sql = require('mssql/msnodesqlv8')
 const { sqlServer: db } = require('../config')
 
@@ -10,12 +10,12 @@ module.exports = async () => {
     requestTimeout: 30 * 60 * 1000,
     options: {
       enableArithAbort: true,
-      encrypt: true
+      encrypt: true,
+      trustedConnection: true
     }
   }
 
-  config.options.trustedConnection = true
-  config.connectionString = 'Driver={ODBC Driver 17 for SQL Server};Server={#{server},#{port}};Database={#{database}};Trusted_Connection={#{trusted}};'
+  config.connectionString = `Driver={ODBC Driver 17 for SQL Server};Server=${db.hostname};Database=${db.database};Trusted_Connection=yes;`
 
   return await sql.connect(config)
 }
